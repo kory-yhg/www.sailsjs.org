940e58cef20b4c8653308655e877d58dec406f0d
Mike McNeil <michael.r.mcneil@gmail.com>
2014-03-17 17:46:29 -0500

    Decorate emoji charts (parser now uses jsdom+jquery to transform HTML).

--- parser.js
+++ parser.js
@@ -58,14 +58,25 @@ var Ejs = function Ejs(savePath, md) {
 	var convertToHTML = require('marked');
 
 	if (md.substr(0,1) !== '#')
-		md = '#'+md
+		md = '#'+md;
 
 	convertToHTML(md, muOptions, function finishedConverting (err, html) {
 		if (err) throw err;
-//		console.log('Writing '+savePath+'.html of size '+html.length);
-		fs.outputFile(savePath + '.html', html, function writeCB(err) {
-			if (err) return new Error('rrrrrrrr'+err);
+
+		// Perform some HTML transformations:
+		html = transformHTML(html, function (errors, transformedHTML) {
+			if (errors) throw errors;
+
+			fs.outputFile(savePath + '.html', transformedHTML, function writeCB(err) {
+				if (err) {
+					console.error('Error writing file:', err);
+					// throw new Error('rrrrrrrr'+err);
+				}
+
+				// If successul, do nothing.
+			});
 		});
+
 	});
 };
 
@@ -279,3 +290,61 @@ process.on('exit', function makeSiteMap() {
 	siteMap.generate();
 	console.log('Sitemap written.  Exiting');
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * Transform some stuff in the HTML.
+ *
+ *  - turn emoji into relevant classes on their containers
+ *
+ * @param  {[type]} html [description]
+ */
+function transformHTML (html, cb) {
+	// td.yes
+	// td.notyet.no
+	// td.never
+
+	// - :white_check_mark: - supported feature
+	//  - :white_large_square: - feature not yet implemented for this transport
+	//  - :heavy_multiplication_x: - unsupported feature due to protocol restrictions
+	//  
+
+	var JQUERY = require('jquery');
+
+	// first argument can be html string, filename, or url
+	require('jsdom').env(html, function (errors, window) {
+		if (errors) return cb(errors);
+		
+		var $ = JQUERY(window);
+
+		//
+		// Replace text nodes and add CSS class(es)
+		// 
+		
+		$('td:contains("white_check_mark")').addClass('yes');
+		$('td:contains("white_check_mark")').text('');
+
+		$('td:contains("white_large_square")').addClass('no').addClass('notyet');
+		$('td:contains("white_large_square")').text('');
+
+		$('td:contains("heavy_multiplication_x")').addClass('never');
+		$('td:contains("heavy_multiplication_x")').text('');
+
+		// Convert transformed HTML into a string and send it back
+		var transformedHTML = window.document.documentElement.outerHTML;
+		cb(null, transformedHTML);
+	});
+
+}
