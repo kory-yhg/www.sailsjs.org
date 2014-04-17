3432a294defc0aeb150e93b7b1906d51fcc44b19
uncletammy <uncletammy@gmail.com>
2014-04-17 18:55:12 -0500

    /refresh now uses docTemplater module instead of spawning child process

--- api/controllers/RefreshController.js
+++ api/controllers/RefreshController.js
@@ -2,8 +2,8 @@ module.exports = {
 
 	index: function(req,res){
 
+/*
 		var childProcess = require('child_process');
-
 		try {
 
 
@@ -37,9 +37,55 @@ module.exports = {
 		} catch(err) {
 			res.send('Uh Oh. There was an error running the parser:'+err)
 		}
+*/
 
 
+		var templater = require('docTemplater');
 
+		var afterTemplateCB = function(err,stuff){
+					if (err){
+						console.log('There was at least one error bro'+err)
+					} else {
+						console.log('No errors.  WOOOO!');
+						res.send('Sucesfull Stuff:'+JSON.stringify(stuff)+'<br>Errors:'+JSON.stringify(err))
+					}
+		};
+
+		// This function is applied to each template before the markdown is converted to markup
+		var beforeConvert = function(writeFileObject,cb){
+			return cb(writeFileObject)
+
+		}
+		// This function is applied to each template after the markdown is converted to markup
+		var afterConvert = function(writeFileObject,cb){
+			//writeFileObject.templateHTML = '*|'+writeFileObject.fullPathAndFileName+'|*\n'+writeFileObject.templateHTML;
+			return cb(writeFileObject)
+		};
+
+		var parseThese = [/*{
+				docsGitRepo: 'git://github.com/balderdashy/sails-docs-guides.git',
+				prependPathAndName: true,
+				addToSitemap: false,
+				parsedTemplatesDirectory: 'assets/templates/guides/'
+			},*/{
+				docsGitRepo: 'git://github.com/balderdashy/sails-docs.git',
+				dirNameInRepo: 'reference',
+				addToSitemap: true,
+				parsedTemplatesDirectory: 'assets/templates/reference/',
+				applyToTemplates:{
+					beforeConvert: beforeConvert,
+					afterConvert: afterConvert
+				}
+			},{
+				docsGitRepo: 'git://github.com/balderdashy/sails-docs.git',
+				dirNameInRepo: 'anatomy',
+				prependPathAndName: false,
+				addToSitemap: true,
+				parsedTemplatesDirectory: 'assets/templates/anatomy/'
+			}];
+
+
+		templater.createTemplate(parseThese,afterTemplateCB);
 
 	}
 
