5ccfe827c9c39976bb41bb4027b6c5377f21222b
KallunaLovegeed <heather@balderdash.co>
2014-05-21 11:46:26 -0500

    parallax design tweaks on get started page

--- api/services/refreshDocsOnStartup.js
+++ api/services/refreshDocsOnStartup.js
@@ -1,10 +1,10 @@
-module.exports = function refreshDocsOnStartup(sails){
-	sails.after(['hook:services:loaded'], function(){
-		DocCompiler(function(err,cb){
-			if (err)
-				return err
-			else
-				console.log('Docs pulled from Github!')
-		})
-	});
-}(sails)
+// module.exports = function refreshDocsOnStartup(sails){
+// 	sails.after(['hook:services:loaded'], function(){
+// 		DocCompiler(function(err,cb){
+// 			if (err)
+// 				return err
+// 			else
+// 				console.log('Docs pulled from Github!')
+// 		})
+// 	});
+// }(sails)
