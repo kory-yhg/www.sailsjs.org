1fbca4f617692984f13f108e2eb9e2247e121e34
Mike McNeil <mike@balderdash.co>
2014-07-01 19:47:59 -0500

    Added loading state to news banner, more cleanup

--- api/services/GithubRepo.js
+++ api/services/GithubRepo.js
@@ -5,6 +5,13 @@
 var Github = require('github');
 var switchback = require('node-switchback');
 
+//**********************************************************************
+//
+// NOTE:
+// CURRENTLY UNUSED!!!!
+//
+//**********************************************************************
+
 
 // Constants
 var _3HOURS = 1000 * 60 * 60 * 3;
