d385cb47b3d519a64be2b981a75704102e7feccc
Mike McNeil <mike@balderdash.co>
2014-07-01 19:39:47 -0500

    Remove logs on frontend

--- assets/js/controls/SupportCtrl.js
+++ assets/js/controls/SupportCtrl.js
@@ -3,20 +3,18 @@ angular.module('Sails').controller('SupportCtrl', [
   '$window',
   '$location',
   function($scope, $window, $location) {
-  	console.log('SupportCtrl HITTING!')
+
     // Houses the state for the support pages.
     // Should never be reset (only its properties changed)
     $scope.support = {};
-// $scope.$on('$locationChangeSuccess', function(event) {
+
     $scope.support.sectionTemplate = './templates/pages/support/supportDefault.html';
-	$scope.intent = angular.extend($scope.intent || {}, {
-	  loadSubSection: function(templateName){
-	      console.log('loading subsection:',templateName);
-	      console.log($scope.support)
-	  	$scope.support.sectionTemplate = './templates/pages/support/'+templateName+'.html';
-	      // window.location.hash = 'support/'+templateName;
-	  }
-	});
+
+    $scope.intent = angular.extend($scope.intent || {}, {
+      loadSubSection: function(templateName) {
+        $scope.support.sectionTemplate = './templates/pages/support/' + templateName + '.html';
+      }
+    });
 
   }
 ]);
