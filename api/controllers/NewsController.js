0499f006a1491f956922fd37a08d39423dfe32b4
Mike McNeil <mike@balderdash.co>
2014-07-19 13:04:43 -0500

    Only wipe machine cache when bootstrapping in production env.  Also use separate module for gh integration details instead of inline code

--- api/controllers/NewsController.js
+++ api/controllers/NewsController.js
@@ -13,13 +13,47 @@ module.exports = {
    */
   find: function(req, res) {
 
-    GetRepoCommits.fn({
-      repo: req.param('repo'),
-      user: req.param('user')
+    // Cache results to avoid exceeding our github rate limit
+    Cache.find()
+    .where({
+      createdAt: {
+        '>': new Date((new Date()) - 1000 * 60 * 60 * 3)
+      },
+      user: 'balderdashy',
+      repo: 'sails'
     })
-    .on('error', res.serverError)
-    .on('success', function (repo) {
-      res.json(repo);
+    .sort('createdAt DESC')
+    .limit(1)
+    .exec(function(err, cached) {
+      if (err) return res.serverError(err);
+
+      if (cached.length) {
+        return res.ok(cached[0].data);
+      }
+
+      require('node-machine')
+      .machine('machinepack-github/get-repo-commits')
+      .configure({
+        user: 'balderdashy',
+        repo: 'sails'
+      })
+      .exec({
+        error: res.serverError,
+        success: function (data){
+
+          // Cache the result
+          Cache.create({
+            user: 'balderdashy',
+            repo: 'sails',
+            data: data
+          })
+          .exec(function(err) {
+            if (err) return res.serverError(err);
+            return res.ok(data);
+          });
+        }
+      });
     });
   }
+
 };
