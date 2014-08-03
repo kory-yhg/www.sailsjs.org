52f9f19a54fa11c18d9f3ac4f7fac0b81862b50d
Mike McNeil <mike@balderdash.co>
2014-08-03 11:26:22 -0500

    Throttle refresh.

--- api/controllers/RefreshController.js
+++ api/controllers/RefreshController.js
@@ -2,12 +2,24 @@
  * Used for compiling markdown documentation into HTML templates.
  * @type {Object}
  */
+
 module.exports = {
 
   index: function(req, res) {
-    DocCompilerService(function (err, metadata) {
-      if (err) return res.serverError(err);
-      return res.ok();
-    });
+
+    require('node-machine')
+    .build({
+      fn: function($i, $x) {
+        DocCompilerService($x);
+      }
+    })
+    .configure({})
+    // Throttle how often the docs can be refreshed
+    // (i.e. no more than once per minute)
+    .cache({
+      model: Cache,
+      ttl: 60000
+    })
+    .exec(res.respond);
   }
 };
