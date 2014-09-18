cfa88802369d0e2d9cca7df70a9b441169018a5c
Mike McNeil <mike@balderdash.co>
2014-09-17 22:23:31 -0500

    Rm -rf the contents of the .tmp/phantomjscloud dir to cleanup.  Still running into timeout issues w/ the service though...

--- api/services/prepare-html-for-google.js
+++ api/services/prepare-html-for-google.js
@@ -6,6 +6,7 @@ var _ = require('lodash');
 var Path = require('path');
 var Hat = require('hat');
 var async = require('async');
+var Filesystem = require('machinepack-fs');
 var fsx = require('fs-extra');
 var PhantomJSCloud = require('machinepack-phantomjscloud');
 
@@ -34,76 +35,84 @@ module.exports = function prepare_html_for_google (options, cb) {
     return cb(new Error('`apiKey` required'));
   }
 
-  // Start off with the hard-coded pages:
-  var urls = [
-    'http://sailsjs.org',
-    'http://sailsjs.org/#!getStarted',
-    'http://sailsjs.org/#!features',
-    'http://sailsjs.org/#!support',
-  ];
-
-  // Then grab the list of generated pages from the various jsmenus...
-  async.each([
-    Path.resolve(__dirname, '../../assets/templates/jsmenus/anatomy.jsmenu'),
-    Path.resolve(__dirname, '../../assets/templates/jsmenus/concepts.jsmenu'),
-    Path.resolve(__dirname, '../../assets/templates/jsmenus/reference.jsmenu'),
-  ], function (jsmenuPath, next) {
-    fsx.readJSON(jsmenuPath, function (err, jsmenu){
-      if (err) return next(err);
-
-      // Build URLs from the jsmenu
-      jsmenu = _.map(jsmenu, function (jsmenuItem) {
-        if (!jsmenuItem.url) {
-          // TODO: trim leading slash from `jsmenuItem.fullPathAndFileName`
-          // build `url` from `fullPathAndFileName` if necessary
-          jsmenuItem.url = 'http:/sailsjs.org/#!documentation/' + (jsmenuItem.realPath || jsmenuItem.fullPathAndFileName);
-        }
-        return jsmenuItem;
-      });
-
-      // then push them onto `urls`
-      urls = urls.concat(_.pluck(jsmenu, 'url'));
-
-      next();
-    });
-  }, function (err) {
+  // First, `rm -rf` existing cached HTML pages:
+  Filesystem.rmrf({
+    dir: Path.resolve(__dirname,'../../.tmp/phantomjscloud')
+  }, function (err){
     if (err) return cb(err);
 
-    console.log('THE FULL LIST OF URLS TO BE FETCHED:', urls);
+    // Start off with the hard-coded pages:
+    var urls = [
+      'http://sailsjs.org',
+      'http://sailsjs.org/#!getStarted',
+      'http://sailsjs.org/#!features',
+      'http://sailsjs.org/#!support',
+    ];
 
-    // Now render the pages using the hosted phantomjs instance(s)
-    PhantomJSCloud.renderPages({
-      apiKey: options.apiKey,
-      urls: urls
-    }).exec(function(err, webpages){
+    // Then grab the list of generated pages from the various jsmenus...
+    async.each([
+      // Path.resolve(__dirname, '../../assets/templates/jsmenus/anatomy.jsmenu'),
+      // Path.resolve(__dirname, '../../assets/templates/jsmenus/concepts.jsmenu'),
+      // Path.resolve(__dirname, '../../assets/templates/jsmenus/reference.jsmenu'),
+    ], function (jsmenuPath, next) {
+      fsx.readJSON(jsmenuPath, function (err, jsmenu){
+        if (err) return next(err);
+
+        // Build URLs from the jsmenu
+        jsmenu = _.map(jsmenu, function (jsmenuItem) {
+          if (!jsmenuItem.url) {
+            // TODO: trim leading slash from `jsmenuItem.fullPathAndFileName`
+            // build `url` from `fullPathAndFileName` if necessary
+            jsmenuItem.url = 'http:/sailsjs.org/#!documentation/' + (jsmenuItem.realPath || jsmenuItem.fullPathAndFileName);
+          }
+          return jsmenuItem;
+        });
+
+        // then push them onto `urls`
+        urls = urls.concat(_.pluck(jsmenu, 'url'));
+
+        next();
+      });
+    }, function (err) {
       if (err) return cb(err);
 
-      console.log('Fetched pages: ',_.pluck(webpages, 'url'));
-      console.log();
+      console.log('THE FULL LIST OF URLS TO BE FETCHED:', urls);
 
-      // Generate an id for each webpage
-      webpages = _.map(webpages, function (webpage) {
-        webpage.id = Hat();
-        return webpage;
-      });
-
-      // Write each webpage to disk using its unique id as its filename
-      async.each(webpages, function (webpage, next){
-        fsx.outputFile(Path.resolve(__dirname,'../../.tmp/phantomjscloud/'+webpage.id), webpage.html, next);
-      }, function (err){
+      // Now render the pages using the hosted phantomjs instance(s)
+      PhantomJSCloud.renderPages({
+        apiKey: options.apiKey,
+        urls: urls
+      }).exec(function(err, webpages){
         if (err) return cb(err);
 
-        // Finally, save the id and url of each webpage in a JSON file
-        fsx.outputJSONSync(Path.resolve(__dirname,'../../.tmp/phantomjscloud/webpages.json'), {
-          createdAt: new Date(),
-          webpages: _.map(webpages, _.partialRight(_.pick, 'id', 'url'))
+        console.log('Fetched pages: ',_.pluck(webpages, 'url'));
+        console.log();
+
+        // Generate an id for each webpage
+        webpages = _.map(webpages, function (webpage) {
+          webpage.id = Hat();
+          return webpage;
+        });
+
+        // Write each webpage to disk using its unique id as its filename
+        async.each(webpages, function (webpage, next){
+          fsx.outputFile(Path.resolve(__dirname,'../../.tmp/phantomjscloud/'+webpage.id), webpage.html, next);
+        }, function (err){
+          if (err) return cb(err);
+
+          // Finally, save the id and url of each webpage in a JSON file
+          fsx.outputJSONSync(Path.resolve(__dirname,'../../.tmp/phantomjscloud/webpages.json'), {
+            createdAt: new Date(),
+            webpages: _.map(webpages, _.partialRight(_.pick, 'id', 'url'))
+          });
+
         });
 
       });
 
     });
-
   });
+
 
 
 };
