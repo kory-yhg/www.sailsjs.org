e772432ed2975d91fdfe18de562d1005c5ff648e
Mike McNeil <mike@balderdash.co>
2014-10-01 12:04:45 -0500

    Merge

---
+++ api/controllers/DocpageController.js
@@ -0,0 +1,11 @@
+/**
+ * DocpageController
+ *
+ * @description :: Server-side logic for managing docpages
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/models/Docpage.js
@@ -0,0 +1,17 @@
+/**
+* Docpage.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+    title: { type: 'string' },
+    text: { type: 'string' },
+    source: { type: 'string', enum: ['reference', 'anatomy', 'concepts'] },
+    href: { type: 'string' }
+  }
+};
+
