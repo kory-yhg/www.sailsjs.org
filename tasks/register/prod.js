c35a6c7bde75575983a8234a3e2fa4157775ec1d
Cody Stoltman <particlebanana@gmail.com>
2015-05-15 17:23:08 -0500

    setup proper prod tasks and use the correct heroku build pack

--- tasks/register/prod.js
+++ tasks/register/prod.js
@@ -1,12 +1,37 @@
 module.exports = function (grunt) {
-	grunt.registerTask('prod', [
-		'compileAssets',
-		'concat',
-		'uglify',
-		'cssmin',
-		'sails-linker:prodJs',
-		'sails-linker:prodStyles',
-		'sails-linker:devTpl',
-		// 'watch'
-	]);
+
+  // grunt.registerTask('heroku:production', [
+  grunt.registerTask('prod', [
+    'clean:dev',
+    'docCompiler',
+    'jst:dev',
+    'less:dev',
+    'copy:dev',
+    'concat',
+    'uglify',
+    'cssmin',
+    'hash',
+    'copy:dist',
+    'clean:prod',
+    'sails-linker:prodJs',
+    'sails-linker:prodStyles'
+  ]);
+
+  grunt.registerTask('heroku:production', [
+    'clean:dev',
+    'docCompiler',
+    'jst:dev',
+    'less:dev',
+    'copy:dev',
+    'concat',
+    'uglify',
+    'cssmin',
+    'hash',
+    'copy:dist',
+    'clean:prod',
+    'sails-linker:prodJs',
+    'sails-linker:prodStyles'
+  ]);
+
+
 };
