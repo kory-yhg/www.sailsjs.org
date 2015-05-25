9ce96e1805fbc703110f1ecb24e3dc9f9a209ed2
Rachael Shaw <r@rachael.wtf>
2015-05-25 14:17:40 -0500

    Tweets slideshow is back

---
+++ assets/js/controls/HomepageCtrl.js
@@ -0,0 +1,29 @@
+angular.module('SailsWebsite').controller('HomepageCtrl', [
+  '$scope',
+  '$interval',
+  function($scope, $interval) {
+
+    //Set initial tweet in Slideshow
+    $scope.activeTweet = '@rauchg';
+
+    $scope.tweets = ['@androolloyd', '@cbmeeks', '@dylanized', '@FeedJoelPie', '@infil00p', '@JohnnyReading', '@lorefnon', '@rauchg'];
+
+    // Start the slideshow:
+    var tweetCount = $scope.tweets.length,
+        curTweet = 0,
+
+    slideshow = $interval(function() {
+      // If current tweet is end of array, start over
+      if(curTweet >= tweetCount){
+        curTweet = 0;
+      }
+      // Add active class to this item
+      $scope.activeTweet = $scope.tweets[curTweet];
+
+      curTweet++;
+    }, 5000);
+}
+]);
+
+
+
